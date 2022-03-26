6d5d04a7ddd2879b50c827fa5bc2877d466f46c3
Jay Patel <jy439129@dal.ca>
2022-03-26 16:06:20 -0300

    rebased with main

---
+++ models/Wishlist.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const WishlistShcema = new Schema ({
+    _id : mongoose.Schema.Types.ObjectId,
+    userId: String,
+    courseId: String
+})
+
+module.exports = mongoose.model("WishlistCollection", WishlistShcema)
